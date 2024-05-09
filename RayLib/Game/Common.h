a01961fa1e1a0df13e7c0718d3f941713a37a9e1
sidz <Sidzzero@users.noreply.github.com>
2024-05-09 23:23:56 +0530

    Grid selection is done

--- RayLib/Game/Common.h
+++ RayLib/Game/Common.h
@@ -1,13 +1,13 @@
 #pragma once
 #include "raylib.h"
 #include<string>
-
+#include <vector>
 
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 450;
 
-constexpr int ROW = 3;
-constexpr int COLUMN = 3; 
+constexpr int ROW = 5;
+constexpr int COLUMN = ROW;
 constexpr float GRID_SIZE = 100; 
 constexpr float START_POINT = 100;
 
--- RayLib/Game/Game.cpp
+++ RayLib/Game/Game.cpp
@@ -3,74 +3,128 @@
 
 #include "Common.h"
 
+
 Vector2 GetGridPosition(Vector2 a_mousePos)
 {
-    Vector2 result={-5,-5};
+    Vector2 result = { -5,-5 };
     //starting offset to offset + grid+col
 
     //Out of Grid location
-    if (a_mousePos.x< START_POINT || 
+    if (a_mousePos.x< START_POINT ||
         a_mousePos.y<START_POINT ||
-        a_mousePos.x>START_POINT+GRID_SIZE*COLUMN ||
-        a_mousePos.y>START_POINT+GRID_SIZE*ROW)
+        a_mousePos.x>START_POINT + GRID_SIZE * COLUMN ||
+        a_mousePos.y>START_POINT + GRID_SIZE * ROW)
     {
-        return Vector2{-1,-1};
+        return Vector2{ -1,-1 };
     }
-    result.x = floor((a_mousePos.x / ( GRID_SIZE * COLUMN) )/(1.0f/COLUMN));
-    result.y = floor(a_mousePos.y / (GRID_SIZE * ROW) / (1.0f/ROW)) ;
+    result.x = floor((a_mousePos.x / (GRID_SIZE * COLUMN)) / (1.0f / COLUMN));
+    result.y = floor(a_mousePos.y / (GRID_SIZE * ROW) / (1.0f / ROW));
 
     return result;
 }
 
 std::string VectorToString(Vector2 a_Vector)
 {
-    return std::string{ std::to_string(a_Vector.x)+":"+ std::to_string(a_Vector.y) };
+    return std::string{ std::to_string(a_Vector.x) + ":" + std::to_string(a_Vector.y) };
 }
 
-int main(void)
+struct Cell
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_WIDTH, "RL_Minesweeper - powered by RAYLIB");
-    std::string strMousePos;
-    Vector2 mousePos;
-    while (!WindowShouldClose())
+public:
+    Color col = Color{BLUE};
+    bool isBomb = false;
+};
+
+class Game
+{
+public:
+    float Timer = 0;
+    Cell grid[ROW][COLUMN];
+
+    Game()
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+    }
+
+    void Startup()
+    {
+    }
+    void Reset()
+    {
+
+    }
+    void Update()
+    {
         mousePos = GetMousePosition();
 
-       
-        for (int i=0;i<ROW;i++)
+        //debug
+        if (IsMouseButtonPressed(0))
+        {
+            auto tempGridPos = GetGridPosition(mousePos);
+            if (tempGridPos.x != -1 && tempGridPos.y != -1)
+            {
+                grid[(int)tempGridPos.y - 1][(int)tempGridPos.x - 1].col = RED;
+            }
+        }
+    }
+    void Draw()
+    {
+        DrawGrid();
+        //debug
+        strMousePos = std::to_string(mousePos.x) + "," + std::to_string(mousePos.y);;
+        DrawCircleV(mousePos, 10, YELLOW);
+        DrawText(("MousePos:" + VectorToString(mousePos)).c_str(), 0, 300, 20, BLACK);
+        DrawText(("GRID_POS:" + VectorToString(GetGridPosition(mousePos))).c_str(), 0, 400, 20, BLACK);
+    }
+    void Shutdown()
+    {
+
+    }
+private:
+
+    std::string strMousePos;
+    Vector2 mousePos;
+
+    void DrawGrid()
+    {
+        for (int i = 0; i < ROW; i++)
         {
             for (int j = 0; j < COLUMN; j++)
             {
                 //DrawRectangle(0,0,GRID_SIZE,GRID_SIZE,RED);
+
                 DrawRectangleRounded(
-                    Rectangle{START_POINT +(float)j*GRID_SIZE,
-                    START_POINT + (float)i*GRID_SIZE,
+                    Rectangle{ START_POINT + (float)j * GRID_SIZE,
+                    START_POINT + (float)i * GRID_SIZE,
                     GRID_SIZE,
-                    GRID_SIZE}, 0.5f, 6, RED);
+                    GRID_SIZE }, 0.5f, 6, grid[i][j].col);
             }
         }
-        strMousePos = std::to_string(mousePos.x) + "," + std::to_string(mousePos.y);;
-        DrawCircleV(mousePos, 10, BLUE);
-        DrawText(("MousePos:" + VectorToString(mousePos)).c_str(), 0, 300, 20, BLACK);
-        DrawText(("GRID_POS:" + VectorToString(GetGridPosition(mousePos))).c_str(), 0, 400, 20, BLACK);
-       
-        EndDrawing();
     }
 
+};
+
+
+
+int main(void)
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_WIDTH, "RL_Minesweeper - powered by RAYLIB");
+    InitAudioDevice();
+    Game mineSweeper;
+    mineSweeper.Startup();
+
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        mineSweeper.Update();
+        mineSweeper.Draw();
+        EndDrawing();
+    }
+    mineSweeper.Shutdown();
     CloseWindow();
 
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
