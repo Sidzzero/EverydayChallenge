f40283368bdfe9bf9965209d9c64d9fe7a8b7d21
sidz <Sidzzero@users.noreply.github.com>
2024-05-12 21:15:07 +0530

    refactored

--- RayLib/Game/PhysicCommon.h
+++ RayLib/Game/PhysicCommon.h
@@ -21,34 +21,91 @@ public:
 struct Polygon
 {
 public :
-	std::vector<PointMass> vertices;
+	std::vector<PointMass> pointMass;
 	void Draw();
 	void Update(float frameDelta);
+	void CheckCollsion(PointMass a_pointMass);
+	Rectangle CreateBoundBox();
+
 };
 
 void Polygon::Draw()
 {
-	for (int i = 0; i < vertices.size(); i++)
+	for (int i = 0; i < pointMass.size(); i++)
 	{
 		int temp_iNextIndex = i + 1;
-		if (i==vertices.size()-1)
+		if (i==pointMass.size()-1)
 		{
 			temp_iNextIndex = 0;
 		}
 
-		DrawLineEx(vertices[i].pos, vertices[temp_iNextIndex].pos, 5.0f, WHITE);
+		DrawLineEx(pointMass[i].pos, pointMass[temp_iNextIndex].pos, 5.0f, WHITE);
 	}
 }
 
 void Polygon::Update(float frameDelta)
 {
-	for (int i=0;i<vertices.size();i++)
+	return;
+	for (int i=0;i<pointMass.size();i++)
 	{
-		auto tempAccel = Vector2Scale(vertices[i].accel, frameDelta);
-		vertices[i].velocity = Vector2Add(vertices[i].velocity, tempAccel);
-		auto NewVector = Vector2Scale(vertices[i].velocity,frameDelta/ vertices[i].mass);
-		vertices[i].pos = Vector2Add(vertices[i].pos,NewVector);
+		auto tempAccel = Vector2Scale(pointMass[i].accel, frameDelta);
+		pointMass[i].velocity = Vector2Add(pointMass[i].velocity, tempAccel);
+		auto NewVector = Vector2Scale(pointMass[i].velocity,frameDelta/ pointMass[i].mass);
+		pointMass[i].pos = Vector2Add(pointMass[i].pos,NewVector);
 	}
+
+}
+
+void Polygon::CheckCollsion(PointMass a_pointMass)
+{
+
+	for (int i = 0; i < pointMass.size(); i++)
+	{
+		int temp_iNextIndex = i + 1;
+		if (i == pointMass.size() - 1)
+		{
+			temp_iNextIndex = 0;
+		}
+
+	}
+}
+Rectangle Polygon::CreateBoundBox()
+{
+	std::vector<Vector2> points;
+	for (int i=0;i< pointMass.size();i++)
+	{
+		points.push_back(pointMass[i].pos);
+	}
+	//find the min /max X and Y and create a box out of it bro
+	float Xmin = INT_MAX, XMax = INT_MIN, Ymin = INT_MAX, Ymax = INT_MIN;
+	for (int i = 0; i < points.size(); i++)
+	{
+		Vector2 current = points[i];
+		if (Xmin > current.x)
+		{
+			Xmin = current.x;
+		}
+		if (Ymin > current.y)
+		{
+			Ymin = current.y;
+		}
+		if (XMax < current.x)
+		{
+			XMax = current.x;
+		}
+		if (Ymax < current.y)
+		{
+			Ymax = current.y;
+		}
+	}
+	Rectangle result;
+	result.x = Xmin;
+	result.y = Ymin;
+	result.width = XMax - Xmin;
+	result.height = Ymax - Ymin;
+
+	return result;
+
 }
 //END------Polygon--------------------
 //PHYSIC WORLD--------------------
@@ -56,6 +113,7 @@ class PhysicWorld
 {
 public:
 	Polygon polygon;
+	PointMass mousePos;
 	
 	void Setup();
 	void Draw();
@@ -77,17 +135,21 @@ void PhysicWorld::Setup()
 		temp.pos = cubespoint[i];
 		temp.accel = Vector2{ 0,9.8f };
 		temp.velocity = Vector2{ 0,0 };
-		polygon.vertices.push_back(temp);
+		polygon.pointMass.push_back(temp);
 	}
 
 }
 void PhysicWorld::Update()
 {
+	mousePos.pos = GetMousePosition();
 	auto temp = GetFrameTime();
-	polygon.Update(temp);
+	//polygon.Update(temp);// DISABLED THE MOPVEMENT
+	polygon.CheckCollsion(mousePos);
 }
 void PhysicWorld::Draw()
 {
 	polygon.Draw();
+	DrawCircleV(mousePos.pos, 10.0f, ORANGE);
+
 }
 // END-------PHYSIC WORLD--------------------
